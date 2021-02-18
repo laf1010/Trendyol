01fd15928741a62efbd3db6c14898e8671fcfc40
karya.boyraz <karya.poyraz@gmail.com>
2021-02-18 08:20:02 +0300

    lastUpdate

--- src/main/java/Trendyol.java
+++ src/main/java/Trendyol.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import static org.openqa.selenium.By.className;
 
 public class Trendyol {
@@ -11,45 +11,84 @@ public class Trendyol {
         public static void main (String[] args) throws InterruptedException {
 
 
-            System.setProperty("webdriver.chrome.driver","C:/Users/karya/Desktop/Selenium/chromedriver/chromedriver.exe");
-
-            WebDriver driver = new ChromeDriver();
-            driver.get ("https://www.google.com/");
-
-            driver.manage().window().maximize();
+            WebDriver driver = getDriver();
             By aramaAlani =  By.name("q");
-            By aramaButonu = By.name("btnK");
 
-            driver.findElement(aramaAlani).sendKeys("trendyol");
-            Thread.sleep(500);
-            //driver.findElement(aramaAlani).sendKeys(Keys.ENTER);
-            driver.findElements(aramaButonu).get(0).click();
-            //driver.findElement(By.className("gNO89b")).click();
+            trendyolGiris(driver, aramaAlani);
 
-//            driver.findElement(By.cssSelector("div.cfxYMc.JfZTW.c4Djg.MUxGbd.v0nnCb")).click();
-//            driver.findElement(By.cssSelector("a.fancybox-item.fancybox-close")).click();
-            int count=20;
-            while(count>1) {
+            popupKapa(driver);
 
-                try {
-                    driver.findElement(By.cssSelector("a.fancybox-item.fancybox-close")).click();
-                    break;
-                }catch (Exception e) {
-                    Thread.sleep(500);
-                    count--;
-                }
+            login(driver);
 
-            }
+            sepeteEkle(driver);
 
-            driver.findElement(className("link-text")).click();
-            driver.findElement(By.id("login-email")).sendKeys("karya.poyraz@gmail.com");
-            driver.findElement(By.id("login-password-input")).sendKeys("111111111");
-            driver.findElement(By.xpath("//button [@type='submit']")).click();
-            Thread.sleep(1000);
-            driver.findElement(className("search-box")).sendKeys("Klavye");
-            driver.findElement(className("search-icon")).click();
+            kuponGiris(driver);
+
+            sepetSil(driver);
+
             Thread.sleep(5000);
             driver.close ();
 
         }
+
+    private static void kuponGiris(WebDriver driver) throws InterruptedException {
+        driver.findElement(By.cssSelector("#Kod")).sendKeys("Kod Girildi");
+        driver.findElement(By.cssSelector("#addDiscountButton")).click();
+        driver.findElement(By.cssSelector(".buttonclose")).click();
+        Thread.sleep(2000);
+    }
+
+    private static void login(WebDriver driver) throws InterruptedException {
+        driver.findElement(className("link-text")).click();
+        driver.findElement(By.id("login-email")).sendKeys("karya.poyraz@hotmail.com");
+        driver.findElement(By.id("login-password-input")).sendKeys("karya1122");
+        driver.findElement(By.xpath("//button [@type='submit']")).click();
+        Thread.sleep(2000);
+    }
+
+    private static void sepeteEkle(WebDriver driver) {
+        driver.findElement(className("search-box")).sendKeys("Klavye");
+//            driver.findElement(className("search-box")).sendKeys(Keys.ENTER);
+        driver.findElement(className("search-icon")).click();
+        driver.findElement(By.cssSelector("div.btn-basket>:nth-child(1)")).click();
+        driver.findElement(By.xpath("//a[@href='/sepetim#/basket']")).click();
+    }
+
+    private static WebDriver getDriver() {
+        System.setProperty("webdriver.chrome.driver","C:/Users/karya/Desktop/Selenium/chromedriver/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get ("https://www.google.com/");
+
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    private static void trendyolGiris(WebDriver driver, By aramaAlani) throws InterruptedException {
+        driver.findElement(aramaAlani).sendKeys("trendyol");
+        Thread.sleep(500);
+        driver.findElement(aramaAlani).sendKeys(Keys.ENTER);
+        driver.findElement(By.cssSelector("div.cfxYMc.JfZTW.c4Djg.MUxGbd.v0nnCb")).click();
+    }
+
+    private static void popupKapa(WebDriver driver) throws InterruptedException {
+        int count=10;
+        while(count>1) {
+
+            try {
+                driver.findElement(By.cssSelector("a.fancybox-item.fancybox-close")).click();
+                break;
+            }catch (Exception e) {
+                Thread.sleep(500);
+                count--;
+            }
+
+        }
+    }
+
+    private static void sepetSil(WebDriver driver) {
+        driver.findElement(By.xpath("//a[@href='/sepetim#/basket']")).click();
+        driver.findElement(By.className("i-trash")).click();
+        driver.findElement(By.cssSelector("button.btn-item.btn-remove")).click();
+    }
 }
