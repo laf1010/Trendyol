f9862951d184a8983ee9aa42a6c6a9421a95121b
karya.boyraz <karya.poyraz@gmail.com>
2021-03-06 04:01:59 +0300

    Commit

--- src/main/java/Trendyol.java
+++ src/main/java/Trendyol.java
@@ -2,16 +2,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 import static org.openqa.selenium.By.className;
 
 public class Trendyol {
 
 
 
-        public static void main (String[] args) throws InterruptedException {
+    public static void main (String[] args) throws InterruptedException {
 
 
             WebDriver driver = getDriver();
+
             By aramaAlani =  By.name("q");
 
             trendyolGiris(driver, aramaAlani);
@@ -31,14 +34,15 @@ public class Trendyol {
 
         }
 
-    private static void kuponGiris(WebDriver driver) throws InterruptedException {
+    public static void kuponGiris(WebDriver driver) throws InterruptedException {
+
         driver.findElement(By.cssSelector("#Kod")).sendKeys("Kod Girildi");
         driver.findElement(By.cssSelector("#addDiscountButton")).click();
         driver.findElement(By.cssSelector(".buttonclose")).click();
         Thread.sleep(2000);
     }
 
-    private static void login(WebDriver driver) throws InterruptedException {
+    public static void login(WebDriver driver) throws InterruptedException {
         driver.findElement(className("link-text")).click();
         driver.findElement(By.id("login-email")).sendKeys("karya.poyraz@hotmail.com");
         driver.findElement(By.id("login-password-input")).sendKeys("karya1122");
@@ -46,15 +50,15 @@ public class Trendyol {
         Thread.sleep(2000);
     }
 
-    private static void sepeteEkle(WebDriver driver) {
+    public static void sepeteEkle(WebDriver driver) {
         driver.findElement(className("search-box")).sendKeys("Klavye");
-//            driver.findElement(className("search-box")).sendKeys(Keys.ENTER);
-        driver.findElement(className("search-icon")).click();
+        driver.findElement(className("search-box")).sendKeys(Keys.ENTER);
+//        driver.findElement(className("search-icon")).click();
         driver.findElement(By.cssSelector("div.btn-basket>:nth-child(1)")).click();
         driver.findElement(By.xpath("//a[@href='/sepetim#/basket']")).click();
     }
 
-    private static WebDriver getDriver() {
+    public static WebDriver getDriver() {
         System.setProperty("webdriver.chrome.driver","C:/Users/karya/Desktop/Selenium/chromedriver/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
@@ -64,14 +68,14 @@ public class Trendyol {
         return driver;
     }
 
-    private static void trendyolGiris(WebDriver driver, By aramaAlani) throws InterruptedException {
+    public static void trendyolGiris(WebDriver driver, By aramaAlani) throws InterruptedException {
         driver.findElement(aramaAlani).sendKeys("trendyol");
         Thread.sleep(500);
         driver.findElement(aramaAlani).sendKeys(Keys.ENTER);
         driver.findElement(By.cssSelector("div.cfxYMc.JfZTW.c4Djg.MUxGbd.v0nnCb")).click();
     }
 
-    private static void popupKapa(WebDriver driver) throws InterruptedException {
+    public static void popupKapa(WebDriver driver) throws InterruptedException {
         int count=10;
         while(count>1) {
 
@@ -86,7 +90,7 @@ public class Trendyol {
         }
     }
 
-    private static void sepetSil(WebDriver driver) {
+    public static void sepetSil(WebDriver driver) {
         driver.findElement(By.xpath("//a[@href='/sepetim#/basket']")).click();
         driver.findElement(By.className("i-trash")).click();
         driver.findElement(By.cssSelector("button.btn-item.btn-remove")).click();
